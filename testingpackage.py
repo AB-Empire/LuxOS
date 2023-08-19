ba394d88bf9bdab52a3eb77fc8e211d5a70a6f20
AlvsDias <98051617+AlvsDias@users.noreply.github.com>
2023-08-19 11:18:19 -0300

    Update testingpackage.py

--- testingpackage.py
+++ testingpackage.py
@@ -1,3 +1,6 @@
+# Testing Package for LuxOS 1.0
+# Just a testing package to test your package system. Officially created by the αβic empire.
+
 class lux_pack:
   name="testingpackage"
   version="1.0"
