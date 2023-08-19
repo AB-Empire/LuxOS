cb0541d8e9a023fdc20237b6274ecd7afacc9815
AlvsDias <98051617+AlvsDias@users.noreply.github.com>
2023-08-19 10:57:38 -0300

    Create testingpackage.py

---
+++ testingpackage.py
@@ -0,0 +1,8 @@
+class lux_pack:
+  name="testingpackage"
+  version="1.0"
+  aditionaldata={"datalist"=[None]}
+  def startscript():
+    print("Testing Package loaded sucessfully!")
+
+testingvar="a"
