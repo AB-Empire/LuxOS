56f543b1abca317384607222280802468281e74c
赤い計算機 <98051617+akai-keisanki@users.noreply.github.com>
2023-12-18 18:30:58 -0300

    Update lux-dev-tools.py

--- lux-dev-tools.py
+++ lux-dev-tools.py
@@ -23,16 +23,35 @@ class lux_pack:
         elif splitcmd[1] == "help":
           print(" --- testingpackage help --- ")
           print("cliv - manages the Command Line Interface Variables")
+          print("clit - manages the Command Line Interface Type")
       except:
           print(cmd+" >> dev-tools failed.")
-    elif splitcmd[0] == "cliv":
+
+    elif splitcmd[0] == "cliv" or splitcmd[0] == "clivars":
         try:
-            if splitcmd[1] == "editvar" or splitcmd[1] == "editv" or splitcmd[1] == "evar" or splitcmd[1] == "ev"::
+          if cliv["prestige"] >= 1:
+            if splitcmd[1] == "seevar" or splitcmd[1] == "seev" or splitcmd[1] == "svar" or splitcmd[1] == "sv":
+                print(f"CLIV >> vars >> {splitcmd[2]} = {cliv['vars'][splitcmd[2]]}")
+            elif splitcmd[1] == "editvar" or splitcmd[1] == "editv" or splitcmd[1] == "evar" or splitcmd[1] == "ev":
                 cliv["vars"][splitcmd[2]] = input(f"CLIV >> vars >> {splitcmd[2]} = ")
-            elif splitcmd[1] == "seevar" or splitcmd[1] == "seev" or splitcmd[1] == "svar" or splitcmd[1] == "sv":
-                print(f"CLIV >> vars >> {splitcmd[2]} = {cliv["vars"][splitcmd[2]}")
+            elif splitcmd[1] == "listvars" or splitcmd[1] == "listv" or splitcmd[1] == "lvars" or splitcmd[1] == "lv":
+                print(f"CLIV >> vars = {cliv['vars']}")
+          else:
+            print(cmd+" >> cliv failed. You have no prestige to use it.")
         except:
-            print(cmd+" >> cliv failed.")
+          print(cmd+" >> cliv failed.")
+
+    elif splitcmd[0] == "clit" or splitcmd[0] == "clitype":
+                try:
+                  if cliv["prestige"] >= 1:
+                    if splitcmd[1] == "seetype" or splitcmd[1] == "seet" or splitcmd[1] == "stype" or splitcmd[1] == "st":
+                      print("CLIV >> type = "+cliv["type"])
+                    elif splitcmd[1] == "edittype" or splitcmd[1] == "editt" or splitcmd[1] == "etype" or splitcmd[1] == "et":
+                      cliv["type"] = input("CLIV >> type = ")
+                  else:
+                    print(cmd+" >> clit failed. You have no prestige to use it.")
+                except:
+                  print(cmd+" >> clit failed.")
     else:
       r=0
     return [cliv2, r]
